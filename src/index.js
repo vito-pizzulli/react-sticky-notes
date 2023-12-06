dbc89c1dd921f2e5870c9d57e786ed70efe11bc1
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-12-06 12:53:17 +0100

    installed sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import '../src/style.scss';
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
