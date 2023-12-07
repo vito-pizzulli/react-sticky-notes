a45458a21e39d599f03a03a3170a57eb9f1636e2
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-12-07 11:39:14 +0100

    added note object with useState hook, handleChange function to keep the object updated when user modifies fields, submitNote passing the note object to App component via props to add it to the array, clearFields to reset the fields

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault()
+    }
+
+    function clearFields(event) {
+        setNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault()
+    }
+
     return (
         <div className="create-area-container">
             <form className="create-area-form">
                 <div className="inputs">
-                    <input type="text" name="note-name" id="note-description" placeholder="Insert title" />
-                    <textarea name="note-description" id="note-description" placeholder="Insert description"></textarea>
+                    <input
+                        type="text"
+                        name="title"
+                        onChange={handleChange}
+                        value={note.title}
+                        placeholder="Title"
+                    />
+                    <textarea
+                        name="content"
+                        onChange={handleChange}
+                        value={note.content}
+                        placeholder="Write your note..."
+                    />
                 </div>
                 <div className="buttons">
-                    <button>Add</button>
-                    <button>Clear fields</button>
+                    <button onClick={submitNote}>Add</button>
+                    <button onClick={clearFields}>Clear fields</button>
                 </div>
             </form>
         </div>
