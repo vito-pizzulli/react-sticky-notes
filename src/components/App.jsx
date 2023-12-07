ef304525bf69a5535bee5f29c9725035c22d484e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-12-07 11:36:57 +0100

    added notes array with useState hook, added addNote & deleteNote functions, added addNote function passing to CreateArea component, deleteNote function & notes array passing to NotesArea component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import CreateArea from "./CreateArea";
 import NotesArea from "./NotesArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteElement, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
       <main>
-        <CreateArea />
-        <NotesArea />
+        <CreateArea onAdd={addNote} />
+        <NotesArea 
+          notes={notes}
+          onDelete={deleteNote}
+        />
       </main>
     </div>
   );
